62b04a0a703cd352b84957d37b7a2611e37ae01e
PierreLemmel <pierre.lemmel@gmail.com>
2018-06-18 18:13:32 +0200

    Setting up callbacks in MainWindow

--- src/gui.py
+++ src/gui.py
@@ -5,9 +5,37 @@ class MainWindow(tk.Frame):
 	def __init__(self, parent):
 		tk.Frame.__init__(self, parent);
 		
+		self.__simpleTextSubmittedCallback = None;
+		self.__timedTextSubmittedCallback = None;
+		self.__theEndSubmittedCallback = None;
+
 		row = 0;
 		self.__initializeSimpleTextRow(row);
 		row += 1;
+		self.__initializeTimedTextRow(row);
+		row += 1;
+		self.__initializeTheEndRow(row);
+
+
+
+	def OnSimpleTextSubmitted(self, callback):
+		self.__funcCheck(callback);
+		self.__simpleTextSubmittedCallback = callback;
+
+
+	def OnTimedTextSubmitted(self, callback):
+		self.__funcCheck(callback);
+		self.__timedTextSubmittedCallback = callback;
+
+
+	def OnTheEndSubmitted(self, callback):
+		self.__funcCheck(callback);
+		self.__theEndSubmittedCallback = callback;
+
+
+	def __funcCheck(self, func):
+		if not callable(func):
+			raise ValueError('The input parameter is not a function');
 
 
 	def __initializeSimpleTextRow(self, row):
@@ -26,5 +54,30 @@ class MainWindow(tk.Frame):
 		col += 1;
 
 
+	def __initializeTimedTextRow(self, row):
+		col = 0;
+		pass;
+
+
+	def __initializeTheEndRow(self, row):
+		col = 0;
+		pass;
+
+
 	def __onSimpleTextClick(self):
-		print('onSimpleTextClick');
+		# @TODO: get text value
+		text = "OnSimpleTextClick";
+		if self.__simpleTextSubmittedCallback is not None:
+			self.__simpleTextSubmittedCallback(text);
+
+
+	def __onTimedTextClick(selft):
+		# @TODO: get text value
+		text = "OnTimedTextClick";
+		if self.__timedTextSubmittedCallback is not None:
+			self.__timedTextSubmittedCallback(text);
+
+
+	def __onTheEndButtonClick(self):
+		if self.__theEndSubmittedCallback is not None:
+			self.__simpleTextSubmittedCallback();
